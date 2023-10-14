09c57e4f7a19a9c73821f6e2d316dfefebca9abe
ronanoconnell <ronanoconnell1971@gmail.com>
2023-10-14 13:39:21 +0100

    Initial Spring Initializr version of the project.

---
+++ src/main/java/uk/co/keyoflife/standaloan/StandALoanApplication.java
@@ -0,0 +1,13 @@
+package uk.co.keyoflife.standaloan;
+
+import org.springframework.boot.*;
+import org.springframework.boot.autoconfigure.*;
+
+@SpringBootApplication
+public class StandALoanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StandALoanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/uk/co/keyoflife/standaloan/StandALoanApplicationTests.java
@@ -0,0 +1,13 @@
+package uk.co.keyoflife.standaloan;
+
+import org.junit.jupiter.api.*;
+import org.springframework.boot.test.context.*;
+
+@SpringBootTest
+class StandALoanApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
